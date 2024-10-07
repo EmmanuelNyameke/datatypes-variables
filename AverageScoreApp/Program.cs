6cee93bdf10942c0e1cafe680545a7534f91516a
EmmanuelNyameke <155826955+EmmanuelNyameke@users.noreply.github.com>
2024-10-07 03:20:38 +0000

    Add files via upload

---
+++ AverageScoreApp/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Program Name: Average Score App
+// Date: September 28, 2024
+// Author: K Bola
+// Purpose: This Application calculates the average score of five exams
+namespace AverageScoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double score1, score2, score3, score4, score5;
+            Console.Write("Enter the first Score: ");
+            score1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the second Score: ");
+            score2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the third Score: ");
+            score3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the fourth Score: ");
+            score4 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the fifth Score: ");
+            score5 = Convert.ToDouble(Console.ReadLine());
+            double sum = score1 + score2 + score3 + score4 + score5;
+            double averageScore = sum / 5;
+            string formatted = averageScore.ToString("N");
+            Console.WriteLine("{0, -5:G} {1, 5:G}", "Scores", "Average Scores");
+            Console.WriteLine("{0, -5:N} {1, 5:N}", sum, averageScore);
+        }
+    }
+}
---
+++ GramsToPoundsApp/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Program Name: Grams To Pounds Converter
+// Date: September 29, 2024
+// Author: K Bola
+// Purpose: This application converts grams to pounds
+namespace GramsToPoundsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pricePerProduct = 2.09;
+            Console.WriteLine("Enter the name of the product: ");
+            string productName = Console.ReadLine();
+            Console.WriteLine($"Enter the unit of {productName}(in grams): ");
+            int grams = Convert.ToInt32(Console.ReadLine());
+            double pounds = grams * 0.00220462;
+            double price = pounds * pricePerProduct;
+            Console.WriteLine($"Product Name: {productName}");
+            Console.WriteLine($"Grams: {grams}");
+            Console.WriteLine($"Price Per Product: {pricePerProduct.ToString("C")}");
+            Console.WriteLine($"Product in pounds: {pounds.ToString("F2")}");
+            Console.WriteLine($"Total Cost: {price.ToString("C")}");
+        }
+    }
+}
